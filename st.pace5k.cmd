f9c3e6596cf27a85657d440982df181ae87ee0a3
iocuser <iocuser@localhost.localdomain>
2019-03-06 18:39:23 +0100

    First startup script

---
+++ st.pace5k.cmd
@@ -0,0 +1,28 @@
+#@field IPADDR
+#@type STRING
+#IP or hostname of the TCP endpoint.
+#
+#@field PREFIX
+#@type STRING
+#Prefix for EPICS PVs.
+
+require pace5000,develop
+require stream,2.7.14p
+require asyn,4.33.0
+
+epicsEnvSet("SYSTEM", "SES-PREMP:")
+epicsEnvSet("IPADDR", "10.4.0.201")
+epicsEnvSet("DEVICE", "Pctrl-PACE5000-001:")
+epicsEnvSet("ASYNPORT", "$(SYSTEM)-asyn-port")
+
+
+#Specifying the TCP endpoint and port name
+drvAsynIPPortConfigure("$(ASYNPORT)", "$(IPADDR):5025")
+
+epicsEnvSet("STREAM_PROTOCOL_PATH", "$(pace5000_DB)")
+
+#Load your database defining the EPICS records
+#dbLoadRecords(pace5000.db, "P=$(PREFIX), R=:,PORT=$(PREFIX)-asyn-port")
+iocshLoad("$(pace5000_DIR)/pace5000.iocsh","SYS=$(SYSTEM), DEV=$(DEVICE), PORT=$(ASYNPORT)")
+
+iocInit()
---
+++ start-labs-utg-se-pace5k01.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+E3_BIN=/epics/base-7.0.2/require/3.0.4/bin
+IOC=labs-utg-se-cam3
+IOC_PATH=/epics/iocs/cmds/$IOC
+IOC_CMD=st.$IOC.cmd
+PORT=2005
+
+source $E3_BIN/setE3Env.bash
+
+/usr/bin/procServ -f -L /var/log/procServ/$IOC -i ^C^D -c $IOC_PATH $PORT $E3_BIN/iocsh.bash $IOC_PATH/$IOC_CMD
